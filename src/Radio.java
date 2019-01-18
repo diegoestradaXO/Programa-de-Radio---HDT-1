285e4e6767c1a469adf4aef0aae6f1133f4ebcbb
isaonaranjo <35385804+isaonaranjo@users.noreply.github.com>
2019-01-18 01:23:37 -0600

    Add files via upload

--- src/Radio.java
+++ src/Radio.java
@@ -1,24 +1,55 @@
-/*
- *Esta interface sera implementada en distintas clases de radio, contiene las funciones basicas.
+/**
+ * 
+ * Esta es la interfaz, donde estan los metodos a utilizar. 
+ * @author Isabel Ortiz y Diego Estrada. 
+ *
  */
 public interface Radio {
-    //Cambia de encendido a apagado
+
+    /**
+     * Enciende la radio.
+     */
     public void toggle();
-    //Devuelve si esta encendido o apagado
+
+    /**
+     * Cambia de AM a FM y viceversa
+     * @return - retorna true o false 
+     */
     public boolean getState();
-    //Cambia de AM a FM y viceversa
+
+    /**
+     * Aumenta o disminuye la estacion, el parametro sup determina si sube(true) o si baja(false) de estacion
+     */
     public void changeFrequency();
-    //Aumenta o disminuye la estacion, el parametro sup determina si sube(true) o si baja(false) de estacion
+
+    /**
+     * Frecuencia
+     * @param  up - up
+     *  
+     */
     public void changeStation(boolean up);
 
+    /**
+     * @return - retorna la estacion 
+     */
     public boolean getFrequency();
 
+    /**
+     * Guarda estaciones 
+     * @param numButton - boton
+     * 
+     */
     public void saveStation(int numButton);
 
+    /**
+     * Cambia estaciones
+     * @param numButton - boton
+     */
     public void changeStationButton(int numButton);
 
+    /**
+     * @return - retorna la estacion 
+     */
     public double getStation();
-
-
 
 }
